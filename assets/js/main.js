a0c78dc0722d3898c348fbd525def41a7aa7a793
nhatkool5899 <77978284+nhatkool5899@users.noreply.github.com>
2022-09-06 10:36:42 +0700

    two commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$('.btn-search').click(function(){
+    $('.input-search').toggleClass('active');
+})
+
+$('.btn-menu').click(function(){
+    $('.dropdown-menu').toggleClass('navbar-sm-active');
+})
